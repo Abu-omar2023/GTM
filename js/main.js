1c965042eb532073bfe98460db1c459a313b561a
Abu-omar2023 <essamabdallah737@yahoo.com>
2023-10-25 02:13:39 +0300

    goo

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 // add class links for nav-bar
     $('.landing-page header .nav-ul li a').click(function(){
         $(this).addClass('links').parent().siblings().find('a').removeClass('links');
+    });
 //change background imgs for landing pag
 setInterval(function () {
     let imgesarray = ["01.jpg","02.jpg","03.jpg","04.jpg"];
@@ -14,9 +15,9 @@ $(".landing-page").css({
     backgroundImage : 'url("imgs/' + imgesarray[randomNum] + '")'
 });
     console.log(randomNum)
-},10000);
+},50000);
 
-    });
+   
 
    
 
